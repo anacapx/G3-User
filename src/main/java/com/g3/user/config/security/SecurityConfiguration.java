de0e7eaea936c11f9b75c23d07221aa21f854410
lisdrl <lisandre.darioli@gmail.com>
2022-06-13 09:24:19 -0300

    feat(back): creation of SecurityConfiguration and TokenValidationFilter

---
+++ src/main/java/com/g3/user/config/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.g3.user.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+		.anyRequest().authenticated()
+		.and().cors()
+		.and().csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().addFilterBefore(new TokenValidationFilter(), UsernamePasswordAuthenticationFilter.class);
+		
+	}
+}
