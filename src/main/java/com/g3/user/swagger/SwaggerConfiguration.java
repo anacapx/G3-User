5d2c24756b6f173b6bce313f05d4a3bc0955f1c9
ab-costa <costa.alessandroborges@gmail.com>
2022-06-21 19:26:19 -0300

    feat(back): basic error controller exclusion

--- src/main/java/com/g3/user/swagger/SwaggerConfiguration.java
+++ src/main/java/com/g3/user/swagger/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package com.g3.user.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -18,7 +19,7 @@ public class SwaggerConfiguration {
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
