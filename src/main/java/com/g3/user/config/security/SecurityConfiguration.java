fa30838518273259ca901d05a39516fc81daf67d
ab-costa <costa.alessandroborges@gmail.com>
2022-06-21 19:14:08 -0300

    feat(back): swagger configuration

--- src/main/java/com/g3/user/config/security/SecurityConfiguration.java
+++ src/main/java/com/g3/user/config/security/SecurityConfiguration.java
@@ -1,7 +1,11 @@
 package com.g3.user.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,14 +16,26 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-		.anyRequest().authenticated()
-		.and().cors()
-		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().addFilterBefore(new TokenValidationFilter(), UsernamePasswordAuthenticationFilter.class);
-		
+			.anyRequest().authenticated()
+			.and().cors()
+			.and().csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().addFilterBefore(new TokenValidationFilter(), UsernamePasswordAuthenticationFilter.class);		
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+		   .antMatchers(HttpMethod.GET, "/v2/api-docs", "/configuration/ui", 
+		"/swagger-resources/**",  "/configuration/security", "/swagger-ui/*", "/webjars/**");
 	}
 }
