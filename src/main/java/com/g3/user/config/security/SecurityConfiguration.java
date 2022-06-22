ac29c866d395393bed52db41c1efed21bd226ae1
elolourenco <elolourenco@outlook.com>
2022-06-22 13:05:24 -0300

    implementacao-swagger-user git commit -m implementacao-swagger-user git commit -m implementacao-swagger-user

--- src/main/java/com/g3/user/config/security/SecurityConfiguration.java
+++ src/main/java/com/g3/user/config/security/SecurityConfiguration.java
@@ -1,16 +1,26 @@
 package com.g3.user.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
@@ -21,5 +31,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().addFilterBefore(new TokenValidationFilter(), UsernamePasswordAuthenticationFilter.class);
 		
+	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+		   .antMatchers(HttpMethod.GET, "/v2/api-docs", "/configuration/ui", 
+		"/swagger-resources/**",  "/configuration/security", "/swagger-ui/*", "/webjars/**");
 	}
 }
